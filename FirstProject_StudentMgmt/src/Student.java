c0acb90e27b11d5d9ada1e3d0b71591068a5ff2e
Sanjay Degaonkar <sanjay.degaonkar@capgemini.com>
2021-01-05 15:48:25 +0530

    student project

---
+++ FirstProject_StudentMgmt/src/Student.java
@@ -0,0 +1,31 @@
+
+public class Student {
+	
+	private int rollNumber;
+	private String name;
+	
+	private String[] courses;
+	public Student(int rollNumber, String name) {
+		super();
+		this.rollNumber = rollNumber;
+		this.name = name;
+	}
+	public int getRollNumber() {
+		return rollNumber;
+	}
+	public void setRollNumber(int rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+
+}
